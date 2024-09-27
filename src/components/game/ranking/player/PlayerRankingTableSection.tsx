17f761c173bfb1c542ed2a9903caa18f63c066d9
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2024-09-27 11:46:50 +0900

    fix: data가 배열이 아닐 경우 error 표시 이슈 수정

--- src/components/game/ranking/player/PlayerRankingTableSection.tsx
+++ src/components/game/ranking/player/PlayerRankingTableSection.tsx
@@ -9,6 +9,7 @@ import {
   TPlayerRankingColumn,
   TPlayerRankingTable,
 } from '@/types/PlayerRanking';
+import DataTableSkeleton from '@/components/common/ui/table/DataTableSkeleton';
 
 const PlayerRankingTableSection = ({
   rankingType,
@@ -38,12 +39,7 @@ const PlayerRankingTableSection = ({
     }
   }, [activeTab]);
 
-  const {
-    data,
-    delayLoading,
-    isError: isError,
-    error,
-  } = useAxios<
+  const { data, delayLoading, isError, error } = useAxios<
     { data: { list: TPlayerRankingTable[] } },
     TPlayerRankingTable[]
   >({
@@ -71,18 +67,16 @@ const PlayerRankingTableSection = ({
     setActiveTab(title);
   };
 
-  if (isError || !Array.isArray(data)) {
-    return <p>Error: {error}</p>;
-  }
-
   const handleSearch = (searchWord: string) => {
-    if (searchWord.length === 0) {
-      setTableData(data);
-    } else {
-      const filterTable = data.filter((item) =>
-        item.playerName.includes(searchWord),
-      );
-      setTableData(filterTable);
+    if (Array.isArray(data)) {
+      if (searchWord.length === 0) {
+        setTableData(data);
+      } else {
+        const filterTable = data.filter((item) =>
+          item.playerName.includes(searchWord),
+        );
+        setTableData(filterTable);
+      }
     }
   };
 
@@ -102,14 +96,18 @@ const PlayerRankingTableSection = ({
       <p className="w-full text-end text-kt-gray-2">
         ※ 각 항목을 클릭하시면 오름차순/내림차순 정렬이 가능합니다.
       </p>
-      <PlayerRankingTable
-        activeTab={activeTab}
-        tableData={tableData}
-        tableColumns={tableColumns}
-        isLoading={delayLoading}
-        isError={isError}
-        error={error}
-      />
+      {!Array.isArray(data) ? (
+        <DataTableSkeleton />
+      ) : (
+        <PlayerRankingTable
+          activeTab={activeTab}
+          tableData={tableData}
+          tableColumns={tableColumns}
+          isLoading={delayLoading}
+          isError={isError}
+          error={error}
+        />
+      )}
     </section>
   );
 };
