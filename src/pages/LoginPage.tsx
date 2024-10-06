f01ed29dc8e51d3bd8d605280e657d2e8a97af6b
bbjbc <jobc4550@gmail.com>
2024-10-06 23:56:13 +0900

    feat: 로그인 페이지에 카카오 소셜 로그인 앱 등록

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,21 +1,37 @@
-import { Link } from 'react-router-dom';
+import { RiKakaoTalkFill } from 'react-icons/ri';
 
+import AuthPageLayout from '@/components/auth/common/AuthPageLayout';
 import LoginForm from '@/components/auth/form/LoginForm';
 
 const LoginPage = () => {
+  const K_REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API;
+  const K_REDIRECT_URI = 'http://localhost:5173/auth';
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${K_REST_API_KEY}&redirect_uri=${K_REDIRECT_URI}&response_type=code`;
+
+  const handleKakaoLogin = () => {
+    window.location.href = kakaoURL;
+  };
+
   return (
-    <main className="flex h-full flex-col items-center justify-center pt-32">
-      <h1 className="text-xl font-semibold text-white">
-        Kt Wiz에 오신 것을 환영해요!
-      </h1>
+    <AuthPageLayout to="/signup">
+      <div className="mb-6 flex justify-between">
+        <button
+          className="flex w-full items-center justify-center gap-2 rounded-md bg-[#FEE500] px-4 py-2 text-sm font-medium text-gray-700 transition-colors duration-300 hover:bg-[#FEE500]"
+          onClick={handleKakaoLogin}
+        >
+          <RiKakaoTalkFill size={20} />
+          카카오 계정으로 로그인
+        </button>
+      </div>
+
+      <div className="relative mb-6">
+        <hr className="border-gray-300" />
+        <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-2 text-sm text-gray-500">
+          OR
+        </span>
+      </div>
       <LoginForm />
-      <Link
-        to="/signup"
-        className="mt-4 flex justify-end text-sm text-blue-500 transition-all duration-200 hover:text-blue-800"
-      >
-        아직 회원이 아니신가요?
-      </Link>
-    </main>
+    </AuthPageLayout>
   );
 };
 
