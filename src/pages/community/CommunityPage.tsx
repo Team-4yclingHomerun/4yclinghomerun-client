a4b6ce130e4e00f21acd7165c2ed8d45b3c167b5
bbjbc <jobc4550@gmail.com>
2024-10-09 19:35:21 +0900

    feat: 커뮤니티 페이지 기본 레이아웃 추가

--- src/pages/community/CommunityPage.tsx
+++ src/pages/community/CommunityPage.tsx
@@ -1,8 +1,15 @@
+import DetailPageLayout from '@/components/common/layout/DetailPageLayout';
+import topImg from '@/assets/community/top_img.jpg';
+
 const CommunityPage = () => {
   return (
-    <>
-      <h1>Community Page!</h1>
-    </>
+    <DetailPageLayout
+      topImg={topImg}
+      title="KT Wiz 커뮤니티"
+      subTitle="kt wiz 팬들과 함께하는 커뮤니티에요."
+    >
+      <h1>Community Page</h1>
+    </DetailPageLayout>
   );
 };
 
