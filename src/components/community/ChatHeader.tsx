f97d4fab4c26bde4bf690831b77214e03657698d
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2024-10-15 13:56:04 +0900

    fix: 커뮤니티 운영방침 open 버튼 button 공용 컴포넌트로 수정

--- src/components/community/ChatHeader.tsx
+++ src/components/community/ChatHeader.tsx
@@ -2,6 +2,7 @@ import { HiOutlineSpeakerphone } from 'react-icons/hi';
 
 import GuidelinesModalContent from './GuidelinesModalContent';
 import { useModal } from '@/hooks/useModal';
+import { Button } from '../common/ui/button/button';
 
 const ChatHeader = () => {
   const { open, Modal } = useModal();
@@ -9,13 +10,13 @@ const ChatHeader = () => {
   return (
     <>
       <header className="flex items-center justify-end">
-        <button
+        <Button
           onClick={open}
-          className="mb-3 flex h-fit w-fit items-center justify-center gap-2 rounded-md bg-kt-red-2 px-3 py-1 drop-shadow-[0_3px_3px_rgba(0,0,0,0.3)]"
+          className="mb-3 flex items-center justify-center gap-2 rounded-md bg-kt-red-2 drop-shadow-[0_3px_3px_rgba(0,0,0,0.3)]"
         >
           <HiOutlineSpeakerphone size={20} />
           <p>커뮤니티 운영방침</p>
-        </button>
+        </Button>
       </header>
       <Modal>
         <GuidelinesModalContent />
